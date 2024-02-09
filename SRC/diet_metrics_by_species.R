8031822b83d7f4e68349c698d26bdfc2716d676a
Iris George <69059695+iris-george@users.noreply.github.com>
2024-02-09 15:36:56 -0500

    Frequency by species calculation

--- SRC/diet_metrics_by_species.R
+++ SRC/diet_metrics_by_species.R
@@ -47,6 +47,19 @@ frequency_data <- stomach_contents[,c("grouper.ID", "species", "broad.taxa")]
 # subset for only distinct rows
 frequency_data <- distinct(frequency_data, .keep_all = FALSE)
 
+# calculate the number of stomachs for each species 
+species_n <- frequency_data[,c("grouper.ID", "species")]
+species_n <- distinct(species_n)
+
+length(which(species_n$species == "nassau.grouper")) # 55 nassau stomachs
+length(which(species_n$species == "yellowfin.grouper")) # 12 yellowfin stomachs
+length(which(species_n$species == "black.grouper")) # 8 black stomachs 
+length(which(species_n$species == "redhind")) # 19 redhind stomachs 
+length(which(species_n$species == "tiger.grouper")) # 1 tiger stomach
+
+# remove tiger grouper: only 1 stomach with food content 
+frequency_data <- frequency_data[!(frequency_data$species %in% "tiger.grouper"),]
+
 # calculate the number of stomachs containing each diet item
 frequency_data <- frequency_data %>% 
   group_by(species, broad.taxa) %>% 
@@ -58,22 +71,86 @@ taxa_frequency <- distinct(taxa_frequency)
 
 # split into dataframe/species and add average frequency 
 nassau_frequency <- subset(taxa_frequency, species == "nassau.grouper")
-nassau_frequency$mean.frequency <- nassau_frequency$taxa.frequency/sum(nassau_frequency$taxa.frequency) 
+nassau_frequency$mean.frequency <- nassau_frequency$taxa.frequency/55
 
 yellowfin_frequency <- subset(taxa_frequency, species == "yellowfin.grouper")
-yellowfin_frequency$mean.frequency <- yellowfin_frequency$taxa.frequency/sum(yellowfin_frequency$taxa.frequency) 
+yellowfin_frequency$mean.frequency <- yellowfin_frequency$taxa.frequency/12
 
 black_frequency <- subset(taxa_frequency, species == "black.grouper")
-black_frequency$mean.frequency <- black_frequency$taxa.frequency/sum(black_frequency$taxa.frequency) 
+black_frequency$mean.frequency <- black_frequency$taxa.frequency/8
 
 redhind_frequency <- subset(taxa_frequency, species == "redhind")
-redhind_frequency$mean.frequency <- redhind_frequency$taxa.frequency/sum(redhind_frequency$taxa.frequency)
+redhind_frequency$mean.frequency <- redhind_frequency$taxa.frequency/19
 
 tiger_frequency <- subset(taxa_frequency, species == "tiger.grouper")
-tiger_frequency$mean.frequency <- tiger_frequency$taxa.frequency/sum(tiger_frequency$taxa.frequency) 
+tiger_frequency$mean.frequency <- tiger_frequency$taxa.frequency/1 
 
 # merge species dataframes back together 
-avg_taxa_frequency <- rbind(nassau_frequency, yellowfin_frequency, black_frequency, redhind_frequency, tiger_frequency)
+avg_taxa_frequency <- rbind(nassau_frequency, yellowfin_frequency, black_frequency, redhind_frequency)
+
+# add in frequency = 0 rows 
+yellowfin_echinodermata <- data.frame("yellowfin.grouper", "echinodermata", 0, 0)
+names(yellowfin_echinodermata) <- c("species", "broad.taxa", "taxa.frequency", "mean.frequency")
+
+black_crustacea <- data.frame("black.grouper", "crustacea", 0, 0)
+names(black_crustacea) <- c("species", "broad.taxa", "taxa.frequency", "mean.frequency")
+
+black_echinodermata <- data.frame("black.grouper", "echinodermata", 0, 0)
+names(black_echinodermata) <- c("species", "broad.taxa", "taxa.frequency", "mean.frequency")
+
+redhind_echinodermata <- data.frame("redhind", "echinodermata", 0, 0)
+names(redhind_echinodermata) <- c("species", "broad.taxa", "taxa.frequency", "mean.frequency")
+
+avg_taxa_frequency <- rbind(avg_taxa_frequency, yellowfin_echinodermata, black_crustacea, black_echinodermata, redhind_echinodermata)
+
+# export frequency dataframe 
+write.csv(avg_taxa_frequency, file = "taxa_frequency.csv", row.names = FALSE)
+
+
+# Abundance Calculation ========================================================
+
+# Calculate abundance of diet contents: the number of individuals in each 
+# stomach recorded and expressed as a proportion of the total individuals 
+# present in the stomach (%N); the average %N can be calculated across all 
+# stomachs for each grouper species. Calculating for broad taxa by species. 
+
+# In each stomach that contains food, what is the average proportion of those
+# items that belong to a certain taxa? 
+
+# assign new dataframe
+abundance_data <- stomach_contents[,c("grouper.ID", "species", "broad.taxa")]
+
+# calculate total number of species in each stomach
+abundance_data <- abundance_data %>% 
+  group_by(grouper.ID) %>% 
+  add_count(name = "total.n")
+
+# calculate total number of individuals of each taxa in each stomach 
+abundance_data <- abundance_data %>% 
+  group_by(grouper.ID, broad.taxa) %>% 
+  add_count(name = "n.taxa")
+
+# collapse by unique grouper.ID/stomach.item combo
+abundance_data <- distinct(abundance_data)
+
+# calculate proportion of total individuals 
+abundance_data$proportion.n <- abundance_data$n.taxa/abundance_data$total.n
+
+# calculate average abundance of each diet item
+average_abundance <- abundance_data[,c("species", "broad.taxa", "proportion.n")]
+average_abundance <- average_abundance %>% 
+  group_by(species) %>% 
+  summarise(mean.abundance = mean(proportion.n))
+
+
+
+
+
+
+
+
+
+
 
 
   
